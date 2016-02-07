6a2d6c376414722156e5d605d2fd02cf3f5a3f47
Sakari Matikka <sm4tik@sci.fi>
2016-02-07 23:46:40 +0200

    aries: Use the source-built Sony init
    crdroid-devices leo
    commit: 11ef440a19fc56d26f0da3c43e697f5637f4dca4
    
    Thanks to @AdFad666 for the implementations on yukon targets on CM
    
    Signed-off-by: Adam Farden <adam@farden.cz>
    Signed-off-by: David Viteri <davidteri91@gmail.com>
    
    	modified:   BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,8 @@ include device/sony/shinano/PlatformConfig2.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := D5803
 
+TARGET_SPECIFIC_HEADER_PATH += device/sony/aries/include
+
 #Reserve space for data encryption (12656259072-16384)
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12656242688
 
